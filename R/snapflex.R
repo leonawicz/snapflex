823522a1f9dc0f3370c9ac89ff5e02c6ee8061d6
leonawicz <matt.rstat@gmail.com>
2018-01-09 11:39:18 -0700

    Add package documentation.

---
+++ R/snapflex.R
@@ -0,0 +1,11 @@
+#' snapflex: flexdashboards for SNAP data sets.
+#'
+#' Provides functions and templates for generating local flexdashboards showcasing and summarizing SNAP data sets as well as functions for accessing existing popular SNAP flexdashboards hosted online.
+#' \code{snapflex} is a member package in the SNAPverse collection of R packages.
+#'
+#' @docType package
+#' @name snapflex
+NULL
+
+#' @importFrom magrittr %>%
+NULL
