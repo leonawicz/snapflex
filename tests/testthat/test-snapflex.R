52ab5d5bcf7b3fda8217f6aa50e043e92aba72b8
leonawicz <matt.rstat@gmail.com>
2018-01-10 12:29:17 -0700

    Add unit tests.

---
+++ tests/testthat/test-snapflex.R
@@ -0,0 +1,18 @@
+context("snapflex")
+
+test_that("templates are as expected", {
+  expect_is(flex_templates(), "tbl_df")
+  expect_equal(dim(flex_templates()), c(1, 7))
+})
+
+test_that("params are as expected", {
+  expect_is(flex_params("x"), "NULL")
+  expect_is(flex_params("psc1"), "list")
+  expect_equal(flex_params("psc1")[[1]], "location")
+  expect_is(flex_params("psc1")[[2]], "character")
+})
+
+test_that("flex returns as expected", {
+  expect_is(flex("psc1", template_params = list(location = "Fairbanks")), "NULL")
+  expect_error(flex("psc1"), "Additional parameters required. See `flex_params`.")
+})
